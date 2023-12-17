f4ea526b4ef754bbd0d68388e8842f5bc63246b2
Leshechka123 <alexey_2004@inbox.ru>
2023-12-17 20:08:05 +0400

    Docstring for classiterator

--- ClassIterator.py
+++ ClassIterator.py
@@ -1,7 +1,14 @@
 import os
 
-class ClassIterator:
-    def __init__(self, class_label, dataset_path):
+class InstanceIterator:
+    def __init__(self, class_label: str, dataset_path: str):
+        """
+        Класс итератора для обхода экземпляров данных.
+
+        Args:
+            class_label (str): Метка класса, которую необходимо обойти.
+            dataset_path (str): Путь к датасету.
+        """
         self.class_label = class_label
         self.dataset_path = dataset_path
         self.visited_instances = set()
@@ -10,14 +17,29 @@ class ClassIterator:
     def __iter__(self):
         return self
     
-    def __next__(self):
+    def __next__(self) -> str:
+        """
+        Возвращает следующий экземпляр данных.
+
+        Returns:
+            str: Путь к файлу следующего экземпляра данных.
+        
+        Raises:
+            StopIteration: Если больше нет экземпляров данных для обхода.
+        """
         while True:
             self.current_instance = self._get_next_instance()
             if self.current_instance is None:
                 raise StopIteration
             return self.current_instance
     
-    def _get_next_instance(self):
+    def _get_next_instance(self) -> str:
+        """
+        Возвращает путь к следующему экземпляру данных.
+
+        Returns:
+            str: Путь к файлу следующего экземпляра данных.
+        """
         for root, dirs, files in os.walk(self.dataset_path):
             for file in files:
                 if file.endswith(".txt"):
